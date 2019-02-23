2174e8970cfa1ecfb5d09cf131bd5b8488cd19bb
James Dempsey <james.dempsey420@gmail.com>
2019-02-22 19:16:50 -0600

    init

---
+++ client/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Whats going on, manhghghg?</h1>;
+  }
+}
+export default App;

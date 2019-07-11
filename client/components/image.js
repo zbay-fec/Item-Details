47bac1c2eca3b4552038ec364c1c232193c7f76d
Kevin Bench <kevinrbench@gmail.com>
2019-07-11 00:53:00 -0500

    styling added

--- client/components/image.js
+++ client/components/image.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Image(props) {
   if(props && !props.toggle){
-    return <img src={props.image} width='320' height='320'></img>
+    return <img src={props.image} className='main' width='320' height='320'></img>
   }else if(props){
-    return <img src={props.side} width='320' height='320'></img>
+    return <img src={props.side} className='main' width='320' height='320'></img>
   }
 }
--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Sidebar(props) {
   return(props.images.map(x => (
     <div> 
-      <img src={x} width='60' height='60' 
+      <img src={x} width='60' height='60' className='sidepic' id={props.background(x)}
       onMouseOver={props.mouse} onMouseEnter={props.handle} 
       onMouseLeave={props.handle} onClick={props.click}></img>
     </div> 

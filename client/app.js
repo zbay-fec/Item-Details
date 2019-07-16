5bca97661ff651f5ae9ce5b0f516f4acc478c37d
Kevin Bench <kevinrbench@gmail.com>
2019-07-16 14:27:17 -0500

    added componentDidUpdate

--- client/app.js
+++ client/app.js
@@ -83,19 +83,21 @@ class ItemDetail extends React.Component {
     )
   }
 
-  componentDidUpdate () {
-    axios.get(`http://ec2-52-15-148-19.us-east-2.compute.amazonaws.com/item/${this.state.item.data.ID}`)
-    .then(results => {
-      this.setState({item : results});
-      this.setState({images : this.image()});
-      this.setState({main : this.state.images[0]});
-      window.addEventListener('productChanged', e => this.setState({item : {data : {ID : e.detail.id}}}));
-    }
-  )
-    .catch(err => {
-      console.log(err);
-    }
+  componentDidUpdate (prevProps, prevState) {
+    if(this.state.item.data.ID !== prevState.item.data.ID){
+      axios.get(`http://ec2-52-15-148-19.us-east-2.compute.amazonaws.com/item/${this.state.item.data.ID}`)
+      .then(results => {
+        this.setState({item : results});
+        this.setState({images : this.image()});
+        this.setState({main : this.state.images[0]});
+        window.addEventListener('productChanged', e => this.setState({item : {data : {ID : e.detail.id}}}));
+      }
     )
+      .catch(err => {
+        console.log(err);
+      }
+      )
+    }
   }
 
   render () {

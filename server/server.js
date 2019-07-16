e07ba079ab6b94e75c673a98c6eb2ca19c91a586
Kevin Bench <kevinrbench@gmail.com>
2019-07-16 12:25:16 -0500

    cors error handling

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const {Item} = require('../database/index.js');
 const mongoose = require("mongoose");
+const cors = require('cors');
 require('dotenv').config();
 
 mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@zbay-tvguq.mongodb.net/Zbay?retryWrites=true&w=majority`, {useNewUrlParser: true});
@@ -17,6 +18,7 @@ app.use(
     strict: false
   })
 );
+app.use(cors());
 
 app.get("http://ec2-52-15-148-19.us-east-2.compute.amazonaws.com:3002/item/:id", (req, res, next) => {
   Item.findOne({ID : req.params.id})

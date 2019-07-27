beeef7ab0afa7686343107f5fe5752696994376c
zonagilreath <bryson.gilreath@gmail.com>
2019-07-27 16:38:49 -0500

    fix client release, promise return order

--- database/index.js
+++ database/index.js
@@ -9,13 +9,13 @@ module.exports.pool = pool;
 module.exports.get = async function(id){
   let client = await pool.connect();
   return client.query('select * from products where id=$1', [Number(id)])
-  .then(res => res.rows[0])
+  .then(res => {
+    client.release();
+    return res.rows[0]
+  })
   .catch(err => {
     console.log(err);
     console.error(err)
-  })
-  .then(() => {
-    client.release();
   })
 }
 
